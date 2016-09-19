2d3c5fa5dac977050d5e84c624074943e693fc0f
Sebastian Kałużny <skaluzny12@gmail.com>
2016-09-19 15:27:38 +0200

    Fix

--- resources/assets/js/views/goods-received/_form.vue
+++ resources/assets/js/views/goods-received/_form.vue
@@ -6,7 +6,7 @@
         </div>
 
         <div class="field">
-            <label>Data otrzymania (rrrr-mm-ddd)</label>
+            <label>Data otrzymania (rrrr-mm-dd)</label>
             <input type="date" placeholder="Data otrzymania" v-model="form.received_at">
         </div>
 
