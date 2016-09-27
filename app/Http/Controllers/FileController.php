a0b13771b50de04ec9a100c48d722ecd54815a8f
Sebastian Kałużny <skaluzny12@gmail.com>
2016-09-27 16:26:36 +0200

    Add FileController

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\File\FileInterface;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class FileController extends Controller
+{
+
+    /**
+     * @var FileInterface
+     */
+    private $file;
+
+
+    /**
+     * FileController constructor.
+     * @param FileInterface $file
+     */
+    public function __construct(FileInterface $file)
+    {
+        $this->file = $file;
+    }
+
+    private function getFile($id)
+    {
+        $get = $this->file->get($id);
+
+        return $get;
+    }
+
+    public function getDownload($id)
+    {
+        $file = $this->getFile($id);
+
+        return response()->download(storage_path('app/' . $file->path));
+    }
+
+    public function getPreview($id)
+    {
+        $file = $this->getFile($id);
+
+        return response()->file(storage_path('app/' . $file->path));
+    }
+}
