2dd7b8912ff00bc6b90b61240c92ba8622b12529
Sebastian Kałużny <skaluzny12@gmail.com>
2016-11-30 14:22:53 +0100

    Pricing fix font

--- resources/views/pdf/elements-pricing.blade.php
+++ resources/views/pdf/elements-pricing.blade.php
@@ -31,8 +31,8 @@
                             <img src="{{ env('LOGO_FILE') }}" alt="" style="width: 100px;">
                         </td>
                         <td style="text-align: right;">
-                            <h2 style="margin-bottom: 0; font-family: 'Open Sanz'; ">{{ mb_convert_encoding(env('COMPANY_NAME'), 'UTF-8') }}</h2>
-                            <h3 style="margin-top: 5px;">Wycena z dnia {{ $pricing->updated_at->toDateString() }}</h3>
+                            <h2 style="margin-bottom: 0; font-family: 'Open Sans'; ">{{ mb_convert_encoding(env('COMPANY_NAME'), 'UTF-8') }}</h2>
+                            <h3 style="margin-top: 5px; font-family: 'Open Sans';">Wycena z dnia {{ $pricing->updated_at->toDateString() }}</h3>
                         </td>
                     </tr>
                 </table>
